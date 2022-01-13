f6fded399519b503c35251c43cf8a8b9b590ea7d
rodennis <rev.rltodd@gmail.com>
2022-01-13 15:19:05 -0600

    create connection to db

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ from flask import jsonify
 from peewee import *
 from playhouse.shortcuts import model_to_dict
 
+app = Flask(__name__)
+
+db = PostgresqlDatabase('anime', user='postgres', password='', host='localhost', port=5432)
+
+app.run(port=3000, debug=True)
