2229c242ae8edfc334f7d84ae8aef962561bbd5d
rodennis <rev.rltodd@gmail.com>
2022-01-14 10:42:52 -0600

    add more data to facts.sql file

--- facts.sql
+++ facts.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
 CREATE TABLE facts (
-    fact_id text,
-    fact text
+    fact_id varchar(200),
+    fact varchar(200)
 );
 
 COPY facts (show_name, fact) FROM stdin;
@@ -18,6 +18,59 @@ naruto Sanji from One Piece was supposed to be named Naruto.
 naruto Canonically, Naruto s RasenShuriken is a reddish-orange color.
 naruto Naruto and Kurama s Bijuu Bomb is not the same color as the other bijuus.
 naruto Sarutobi was supposed to be a monkey, but the creator of Naruto changed his mind and make him human like other Shinobis. Maybe that is why lord third was able to summon Enma.
+
+AOT Hajime Isyama s favorite character is Jean Kirschtein. Cause Isayma admires how Jean says what he wants without worrying about what other people think. But it was all in the past.
+AOT Originally, Eren was going to know that he is a titan all along. But thankfully, Isayama scrapped the idea.
+AOT Levi came on top in the 1st character popularity poll, Eren took the second place, While our favorite girl, Mikasa took the 3rd place.
+AOT Isayama has said that Mikasa will choose not to wear her precious scarf if it s too hot outside.
+AOT Isayama has revealed that Eren s titan form is based on Yushin Okami, a mixed Japanese martial artist. Isayama felt that he had the ideal physique of a middleweight mixed martial artist.
+AOT Moblit is Hange s second in command as well as her assistant. But thats not all. He is also the heaviest drinker in the survey corps. Isayama has said that this is due to his unfortunate position.
+AOT Marnia Inoue, the voice actor of Armin, is also the voice of the show s narrator.
+AOT Mikasa translates to 3 bamboo hats.
+AOT Isayama has said he received a strange phone call from his following the success of his series. They were curious about the sudden increase of funds in his savings. He postulated they probably thought I was running some bank transfer scam
+AOT Armin s Birthday is on November 3rd, and It suits him. Cause in Japan, The day is celebrated as culture day, promoting academics, culture, and the fine arts.
+AOT Isayama made the theme of AOT cause he thought, Giants are kind of Gross.
+AOT In the Simpsons episode Treehouse of Horror xxv, Lisa was shown dressed as Mikasa. She even used 3D Maneuver Gear.
+AOT Mike is the tallest human character in the series at a looming 6.5.
+AOT It is most likely possible that Erwin Rommel, known as Desert Fox, was served as the main source of inspiration for Erwin Smith.
+AOT Aspects of Levi s design and personality are were based on Rorshach from Watchmen.
+AOT On average, Levi only sleeps around 2 to 3 hours a day.
+AOT Some titans are modeled after friends and acquaintances of Isayama. Sometimes he even takes requests.
+AOT Eren has the highest kill rate of any of the 104 Trainee squad.
+AOT Eren s voice actor Yuki Kaji cried while reading the script of the final season.
+AOT According to Isayama, Erwin stays single because he doesn t know when he ll die. Lucky for the Nile because they were in love with the same woman.
+AOT Isayama revealed that Levi sometimes wishes that he was a bit taller.
+AOT Attack On Titan has 7 episodes rated 9.9 or above. While Breaking Bad and Game Of Thrones have 7 combined.
+AOT Mikasa was the first character Isayama came up with when he was initially developing Attack On Titan.
+AOT Based on statistics from Attack On Titan guide book, Hange and Zeke are the two smartest characters in the series.
+AOT Levi is a Tea enthusiast. He enjoys collecting Tea leaves and is fond of black tea. Isayama has even said that Levi once wanted to open a tea shop.
+
+hunterXhunter The words on Zeno Zoldyk s clothing say either A Kill A Day or Never Retire
+hunterXhunter The Phantom Troupe has an arm wrestling rank among themselves to determine their physical strength. The order from strongest to weakest of this arm wrestling is Uvogin, Phinks, Hisoka, Franklin, Feitan, Machi, Chrollo, Bonolenov, Nobunaga, Shalnark, Pakunoda, Shizuku, and Kortopi.
+hunterXhunter All four main protagonists share the same birthday characteristics â€“ having the same number of birth month and day. And coincidentally, Hisoka too shares the same birthday characteristic. This is the reason why some fans consider him as the fifth protagonist.
+hunterXhunter A character from Yu Yu Hakusho, Suzaku appeared in Chrollo s book as one of his victims.
+hunterXhunter 60 out of the 100 Richest characters in the series are Hunters.
+hunterXhunter The very first character to have appeared in Hunter x Hunter Manga is Kite.
+hunterXhunter Knuckle, Shoot, Palm, and Gyro are named after different baseball pitches Knuckleball, Shoot Ball, Palm ball, and Gyroball.
+hunterXhunter Sui Ishida, the creator of Tokyo Ghoul, is a big fan of Hunter X Hunter. Especially Hisoka.
+hunterXhunter In 1999 Hunter x Hunter anime, Killua watched porno during his stay in a Yorknew City hotel. He even asked Gon to join him. Thankfully, Gon refused the offer.
+hunterXhunter Togashi is married to the author of Sailor Moon, Naoko Takeuchi. She even helped him in earlier chapters.
+hunterXhunter Kurapika is the only known character in the entire HxH series with two Nen types â€“ Conjuration and Specialization.
+hunterXhunter Morel s technique Deep Turtle is a reference to the rock band of the same name.
+hunterXhunter With a 3,250 feet tall,251 floors, Heavens Arena is the 4th tallest building in the Hunter x Hunter series.
+hunterXhunter Hunter x Hunter has its own alphabets which can be translated directly to Japanese then to English.
+hunterXhunter Dwun s room in greed island arc is a reference to a photo of the author s room. Togashi has similar seas of Trash scattered around him while playing the game Dragon Quest.
+hunterXhunter It is shown in the series that Pituo has the biggest En range extending up to 2.2 Kilometers.
+hunterXhunter Killua s favorite food is ChocoRobo-Kun chocolate balls and he hates red peppers. He also has a gambling addiction.
+hunterXhunter Our bald Ninja, Hanzo, is named after Hatori Hanzo, a famous Japanese ninja.
+hunterXhunter Gon shares the same birthday with Monkey D. Luffy, the main protagonist of One Piece.
+hunterXhunter Only Ging Freecss passed the 267th Hunter Examination and it was the same case with Killua but on 288th.
+hunterXhunter Despite his appearances, Kalluto is a boy and is the youngest member of Phantom Troupe.
+hunterXhunter The Greed Island arc is created out of Togashi s desire to draw a card battle manga like Yugioh.
+hunterXhunter Hina s hat imitates her emotions.
+hunterXhunter All Silver-haired members of the Zoldyck family (Zeno, Silva, and Killua) have transmutation type of Nen. On the other hand, the dark-haired members of their family use the Manipulation type.
+hunterXhunter Did you know? Bungee gum has properties of both rubber and gum.
+
 \.
 
 COMMIT;
